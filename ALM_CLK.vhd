22727010736a21592dbd8e2429630499d7ce4887
Shaafihiy <47476897+Shaafihiy@users.noreply.github.com>
2022-02-27 05:36:52 -0800

    digital alarm clock with fpga

---
+++ ALM_CLK.vhd
@@ -0,0 +1,36 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY ALM_CLK IS
+PORT(clk_4 : IN   STD_LOGIC;
+		T3   : OUT  STD_LOGIC
+		);
+END ALM_CLK;
+
+
+ARCHITECTURE Behavior of ALM_CLK IS
+SIGNAL clk    : STD_LOGIC;
+SIGNAL Q, tmp : STD_LOGIC_VECTOR(23 DOWNTO 0);
+
+BEGIN
+	clk <= clk_4;	
+	
+
+   PROCESS(clk, Q) 
+   BEGIN
+		 
+		IF (RISING_EDGE(clk)) THEN
+			tmp <= tmp + 1;
+		END IF;
+		Q <= tmp;	
+		
+		
+		IF (Q <= "000000000000000000000000") THEN
+			T3 <= '1';
+		ELSE
+			T3 <= '0';
+		END IF;
+		
+	END PROCESS;
+END Behavior;
